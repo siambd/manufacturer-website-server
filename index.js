66244c8b9bc88a92876bf7d6fb52735bf2d45125
siam <you@example.com>
2022-05-30 21:30:39 +0530

    update

--- index.js
+++ index.js
@@ -115,3 +115,18 @@ async function run() {
         const bookings = await userInfoCollection.find(query).toArray();
         res.send(bookings);
       })
+
+          //  update fields api
+    app.put('/update', async (req, res) => {
+        const email = req.params.email;
+        const updatedInfo = req.body;
+        const filter = { email: email };
+        const options = { upsert: true }
+        const updatedDoc = {
+          $set: {
+            quantity: updatedInfo.quantity
+          }
+        };
+        const result = await userInfoCollection.updateOne(filter, updatedDoc, options);
+        res.send(result);
+      })
